269d2cad5874205faf8ebe4de9b6ec6914a85be8
felipe-coletti <felipecoletti2@gmail.com>
2022-12-24 15:19:19 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ function toggle() {
     let list = document.querySelector('ul');
     if (list.classList.contains('open')) {
         list.classList.remove('open');
+        list.classList.add('closed');
     } else {
+        list.classList.remove('closed');
         list.classList.add('open');
     }
 }
