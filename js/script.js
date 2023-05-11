defbb3e287a7155c3c2d06ff6db5a431f2359463
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2023-05-11 13:57:04 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 function toggle() {
     let list = document.querySelector('ul');
+    
     if (list.classList.contains('closed')) {
         list.classList.replace('closed', 'opened');
     } else {
